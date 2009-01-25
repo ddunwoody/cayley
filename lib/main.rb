2b54b23b7b00ef40231a32874c7c52e3f198bfea
David Dunwoody <david.dunwoody@gmail.com>
2009-01-25 22:04:49 +0000

    Allow Body to be dynamic, and render from Simulation.bodies list

--- lib/main.rb
+++ lib/main.rb
@@ -17,7 +17,8 @@ POSITION_ITERATIONS = 1
 
 class Simulation
   include Singleton
-  attr_reader :body, :ground, :world, :bodies
+
+  attr_reader :bodies, :world
 
   def initialize
     @bodies = []
@@ -28,58 +29,46 @@ class Simulation
     gravity = B2Vec2.new 0, -10
     do_sleep = true
     @world = B2World.new aabb, gravity, do_sleep
-
-    # Define the dynamic body. We set its position and call the body factory.
-    body_def = B2BodyDef.new
-    body_def.position.Set WIDTH/2, 100
-    @body = @world.CreateBody body_def
-
-    # Define another box shape for our dynamic body.
-    shape_def = B2PolygonDef.new
-    shape_def.SetAsBox 5, 5
-
-    shape_def.density = 1
-    shape_def.friction = 0.3
-    shape_def.restitution = 0.5
-
-    # Add the shape to the body.
-    @body.CreateShape shape_def
-
-    # Now tell the dynamic body to compute it's mass properties base
-    # on its shape.
-    @body.SetMassFromShapes
   end
 
-  def add_body(&block)
-    @bodies << Body.new(&block)
+  def add_body(*args, &block)
+    @bodies << Body.new(*args, &block)
   end
 
   def update
     @world.Step TIMESTEP, VELOCITY_ITERATIONS, POSITION_ITERATIONS
-    # Now print the position and angle of the body.
-    position = @body.GetPosition
-    angle = @body.GetAngle
-    printf "%4.2f %4.2f %4.2f\n", position.x, position.y, angle
   end
 
 end
 
 class Body
-  attr_accessor :position, :colour
+  attr_accessor :colour, :half_width, :half_height,
+    :density, :friction, :restitution
 
-  def initialize
-    body_def = B2BodyDef.new
+  def initialize x, y
     raise 'A block must be given' unless block_given?
     yield self
-    raise 'Position must be set' if position.nil?
-    body_def.position.Set @position[0], @position[1]
-    body = Simulation.instance.world.CreateBody body_def
-    body.CreateShape @shape_def
+    body_def = B2BodyDef.new
+    body_def.position.Set x, y
+
+    @body = Simulation.instance.world.CreateBody body_def
+
+    shape_def = B2PolygonDef.new
+    shape_def.SetAsBox @half_width, @half_height
+    shape_def.density = @density if @density
+    shape_def.friction = @friction if @friction
+    shape_def.restitution = @restitution if @restitution
+
+    @body.CreateShape shape_def
+    @body.SetMassFromShapes if @density
   end
 
   def set_as_box half_width, half_height
-    @shape_def = B2PolygonDef.new
-    @shape_def.SetAsBox half_width, half_height
+    @half_width, @half_height = half_width, half_height
+  end
+
+  def position
+    @body.GetPosition
   end
 end
 
@@ -87,10 +76,17 @@ class MainWindow < Window
   def initialize
     super WIDTH, HEIGHT, false, TIMESTEP
     self.caption = 'Cayley'
-    Simulation.instance.add_body do |body|
-      body.position = WIDTH/2, 20
+    Simulation.instance.add_body(WIDTH/2, 20) do |body|
       body.set_as_box 100, 10
       body.colour = 0xFF7FFF7F
+    end
+
+    Simulation.instance.add_body(WIDTH/2, 100) do |body|
+      body.set_as_box 5, 5
+      body.colour = 0xFFFFFFFF
+      body.density = 1
+      body.friction = 0.3
+      body.restitution = 0.5
     end
   end
 
@@ -99,16 +95,12 @@ class MainWindow < Window
   end
 
   def draw
-    position = Simulation.instance.body.GetPosition
-    x, y = position.x, HEIGHT-position.y
-    c = 0xFFFFFFFF
-    draw_quad x-5, y-5, c, x+5, y-5, c, x-5, y+5, c, x+5, y+5, c
-
     Simulation.instance.bodies.each do |body|
       position = body.position
-      x, y = position[0], HEIGHT-position[1]
+      x, y = position.x, HEIGHT-position.y
+      hw, hh = body.half_width, body.half_height
       c = body.colour
-      draw_quad x-100, y-10, c, x+100, y-10, c, x-100, y+10, c, x+100, y+10, c
+      draw_quad x-hw, y-hh, c, x+hw, y-hh, c, x-hw, y+hh, c, x+hw, y+hh, c
     end
   end
 
