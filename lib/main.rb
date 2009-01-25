eb7770a01004a321c1bc729ad1461372fc3c32eb
David Dunwoody <david.dunwoody@gmail.com>
2009-01-25 21:44:13 +0000

    Define static bodies in more idiomatic manner

--- lib/main.rb
+++ lib/main.rb
@@ -4,6 +4,7 @@ require 'rubygems'
 
 require 'gosu'
 require 'box2d'
+require 'singleton'
 
 include Gosu
 include Box2d
@@ -14,10 +15,12 @@ TIMESTEP = 1/60.0
 VELOCITY_ITERATIONS = 8
 POSITION_ITERATIONS = 1
 
-class World
-  attr_reader :body, :ground
+class Simulation
+  include Singleton
+  attr_reader :body, :ground, :world, :bodies
 
   def initialize
+    @bodies = []
     aabb = B2AABB.new
     aabb.lowerBound.Set 0, 0
     aabb.upperBound.Set WIDTH, HEIGHT
@@ -25,24 +28,6 @@ class World
     gravity = B2Vec2.new 0, -10
     do_sleep = true
     @world = B2World.new aabb, gravity, do_sleep
-
-    # Define the ground body.
-    ground_body_def = B2BodyDef.new
-    ground_body_def.position.Set WIDTH/2, 20
-
-    # Call the body factory which allocates memory for the ground body
-    # from a pool and creates the ground box shape (also from a pool).
-    # The body is also added to the world.
-    @ground = @world.CreateBody ground_body_def
-
-    # Define the ground box shape.
-    ground_shape_def = B2PolygonDef.new
-
-    # The extents are the half-widths of the box.
-    ground_shape_def.SetAsBox 100, 10
-
-    # Add the ground shape to the ground body.
-    @ground.CreateShape ground_shape_def
 
     # Define the dynamic body. We set its position and call the body factory.
     body_def = B2BodyDef.new
@@ -65,13 +50,36 @@ class World
     @body.SetMassFromShapes
   end
 
+  def add_body(&block)
+    @bodies << Body.new(&block)
+  end
+
   def update
     @world.Step TIMESTEP, VELOCITY_ITERATIONS, POSITION_ITERATIONS
-
     # Now print the position and angle of the body.
     position = @body.GetPosition
     angle = @body.GetAngle
     printf "%4.2f %4.2f %4.2f\n", position.x, position.y, angle
+  end
+
+end
+
+class Body
+  attr_accessor :position, :colour
+
+  def initialize
+    body_def = B2BodyDef.new
+    raise 'A block must be given' unless block_given?
+    yield self
+    raise 'Position must be set' if position.nil?
+    body_def.position.Set @position[0], @position[1]
+    body = Simulation.instance.world.CreateBody body_def
+    body.CreateShape @shape_def
+  end
+
+  def set_as_box half_width, half_height
+    @shape_def = B2PolygonDef.new
+    @shape_def.SetAsBox half_width, half_height
   end
 end
 
@@ -79,23 +87,29 @@ class MainWindow < Window
   def initialize
     super WIDTH, HEIGHT, false, TIMESTEP
     self.caption = 'Cayley'
-    @world = World.new
+    Simulation.instance.add_body do |body|
+      body.position = WIDTH/2, 20
+      body.set_as_box 100, 10
+      body.colour = 0xFF7FFF7F
+    end
   end
 
   def update
-    @world.update
+    Simulation.instance.update
   end
 
   def draw
-    position = @world.body.GetPosition
+    position = Simulation.instance.body.GetPosition
     x, y = position.x, HEIGHT-position.y
     c = 0xFFFFFFFF
     draw_quad x-5, y-5, c, x+5, y-5, c, x-5, y+5, c, x+5, y+5, c
 
-    position = @world.ground.GetPosition
-    x, y = position.x, HEIGHT-position.y
-    c = 0xFF80FF80
-    draw_quad x-100, y-10, c, x+100, y-10, c, x-100, y+10, c, x+100, y+10, c
+    Simulation.instance.bodies.each do |body|
+      position = body.position
+      x, y = position[0], HEIGHT-position[1]
+      c = body.colour
+      draw_quad x-100, y-10, c, x+100, y-10, c, x-100, y+10, c, x+100, y+10, c
+    end
   end
 
   def button_down id
