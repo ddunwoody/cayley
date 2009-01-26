131ad4de932d6a1e2ff062c0785570aab92791b6
David Dunwoody <david.dunwoody@gmail.com>
2009-01-26 23:22:07 +0000

    add shell scripts and remove debug_draw from python as it causes crashes

---
+++ python/src/cayley/__init__.py
@@ -0,0 +1,8 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="david"
+__date__ ="$26-Jan-2009 23:09:42$"
+
+if __name__ == "__main__":
+    print "Hello World";
