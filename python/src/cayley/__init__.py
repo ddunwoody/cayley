b5deca4855880d004c2c6e436a78f11b9314129c
David Dunwoody <david.dunwoody@gmail.com>
2009-01-27 18:54:52 +0000

    Be less OO (apparently not a bad thing in python)

--- python/src/cayley/__init__.py
+++
@@ -1,8 +0,0 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
-
-__author__="david"
-__date__ ="$26-Jan-2009 23:09:42$"
-
-if __name__ == "__main__":
-    print "Hello World";
--- python/src/main.py
+++ python/src/main.py
@@ -1,8 +1,9 @@
-from cayley.main_window import MainWindow
-from cayley.simulation import Simulation
+from simulation import Simulation
 from pyglet import app
 from pyglet import clock
-from pyglet.gl import Config
+from pyglet.gl import *
+from pyglet.graphics import *
+from pyglet.window import Window
 
 WIDTH, HEIGHT = 640, 480
 TIMESTEP = 1 / 60.0
@@ -17,7 +18,24 @@ simulation.add_body(position=(WIDTH / 2, 100), dimensions=(10, 10),
 
 config = Config(sample_buffers=1, samples=4, depth_size=16,
                 double_buffer=True)
-MainWindow(simulation, WIDTH, HEIGHT, config)
+window = Window(width=WIDTH, height=HEIGHT, caption='Cayley', config=config)
+
+@window.event
+def on_draw():
+    glClear(GL_COLOR_BUFFER_BIT)
+    for body in simulation.bodies:
+        position = body.position()
+        x, y = position.x, position.y
+        c = body.colour
+        hw, hh = body.dimensions[0] / 2.0, body.dimensions[1] / 2.0
+        draw(4, GL_QUADS,
+             ('v2f', (
+             x - hw, y + hh,
+             x + hw, y + hh,
+             x + hw, y - hh,
+             x - hw, y - hh)),
+             ('c3f', c * 4)
+             )
 
 clock.schedule_interval(simulation.update, TIMESTEP)
 
