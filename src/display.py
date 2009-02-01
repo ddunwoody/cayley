2505ef7ca0b6325895b35aed6fb7e14f374c4d41
David Dunwoody <david.dunwoody@gmail.com>
2009-02-01 17:46:03 +0000

    extract display-specific code to display module

---
+++ src/display.py
@@ -0,0 +1,45 @@
+from pyglet.gl import *
+import pyglet
+
+class Camera:
+    has_moved = False
+    x = y = 0
+    
+    def move_by(self, dx, dy):
+        # don't move for the first event
+        if self.has_moved:
+            self.x += dx
+            self.y += dy
+        else:
+            self.has_moved = True
+        
+class Window(pyglet.window.Window):
+    def __init__(self, simulation, *args, **kwargs):
+        config = Config(sample_buffers=1, samples=4, depth_size=16,
+                        double_buffer=True)
+        super(Window, self).__init__(config=config, *args, **kwargs)
+        self.simulation = simulation
+        self.camera = Camera()
+        self.set_exclusive_mouse()
+
+    def size_to_margin(self, margin):
+        screen = self.screen
+        border_width = int(self.screen.width * margin)
+        border_height = int(self.screen.height * margin)
+        self.width = screen.width - border_width * 2
+        self.height = screen.height - border_height * 2
+
+    def center_on_screen(self):
+        x = (self.screen.width - self.width) / 2
+        y = (self.screen.height - self.height) / 2
+        self.set_location(x, y)
+
+    def on_draw(self):
+        glClear(GL_COLOR_BUFFER_BIT)
+        glLoadIdentity()
+        glTranslatef(self.camera.x, self.camera.y, 0)
+        for body in self.simulation.bodies:
+            body.draw()
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.camera.move_by(dx, dy)
--- src/main.py
+++ src/main.py
@@ -1,57 +1,21 @@
-from pyglet import *
-from pyglet.gl import *
-from pyglet.window import *
+from display import Window
+from pyglet import app, clock
 from simulation import Simulation
 
-TIMESTEP = 1 / 60.0
-MARGIN = 0.1
+simulation = Simulation(10000, 10000)
 
-display = get_platform().get_default_display()
-screen = Display.get_default_screen(display)
-window_scale = 1 - MARGIN * 2
-width = int(screen.width * window_scale)
-height = int(screen.height * window_scale)
-
-simulation = Simulation(width, height)
-
-simulation.add_body(position=(width / 2, 20), dimensions=(200, 20),
+simulation.add_body(position=(500, 20), dimensions=(200, 20),
                     colour=(0.5, 1, 0.5))
 
-simulation.add_body(position=(width / 2, 100), dimensions=(10, 10),
+simulation.add_body(position=(500, 100), dimensions=(10, 10),
                     colour=(1, 1, 1), density=1, friction=0.3, restitution=0.5)
 
 
-config = Config(sample_buffers=1, samples=4, depth_size=16,
-                double_buffer=True)
-
-window = Window(width=width, height=height, caption='Cayley', resizable=True,
-                config=config, visible=False)
-window.set_exclusive_mouse()
-
-class Camera:
-    init = False
-    x = y = 0
-
-camera = Camera()
-
-@window.event
-def on_draw():
-    glClear(GL_COLOR_BUFFER_BIT)
-    glLoadIdentity()
-    glTranslatef(camera.x, camera.y, 0)
-    for body in simulation.bodies:
-        body.draw()
-
-@window.event
-def on_mouse_motion(x, y, dx, dy):
-    if camera.init:
-        camera.x += dx
-        camera.y += dy
-    else:
-        camera.init = True
-
-clock.schedule_interval(simulation.update, TIMESTEP)
-
+window = Window(simulation, caption='Cayley', resizable=True, visible=False)
+window.size_to_margin(0.1)
 window.set_visible()
-window.set_location(int(screen.width * MARGIN), int(screen.height * MARGIN))
+window.center_on_screen()
+
+clock.schedule_interval(simulation.update, 1 / 60.0)
+
 app.run()
