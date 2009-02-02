f3dc2f831f078919927e1613196e263937b11f30
David Dunwoody <david.dunwoody@gmail.com>
2009-02-02 21:39:45 +0000

    extract Shape from Circle and Polygon

--- src/shape.py
+++ src/shape.py
@@ -1,39 +1,41 @@
 from Box2D import b2CircleDef, b2PolygonDef
 
-class Circle:
+class Shape(object):
+    def __init__(self, shape=None, colour=(1,1,1), density=None,
+                 friction=None, restitution=None):
+        self.colour = colour
+        self.shape = shape
+        if density is not None:
+            self.shape.density = density
+        if friction is not None:
+            self.shape.friction = friction
+        if restitution is not None:
+            self.shape.restitution = restitution
+
+    def __str__(self):
+        return "Shape(%s, colour: %s)" % (self.shape.__str__(), self.colour)
+
+
+class Circle(Shape):
     def __init__(self, local_position, radius, colour=(1,1,1),
                density=None, friction=None, restitution=None):
         self.shape = b2CircleDef()
+        super(Circle, self).__init__(shape=b2CircleDef(), colour=colour,
+                                     density=density, friction=friction,
+                                     restitution=restitution)
         self.shape.localPosition = local_position
         self.shape.radius = radius
-        self.colour = colour
-        if density is not None:
-            self.shape.density = density
-        if friction is not None:
-            self.shape.friction = friction
-        if restitution is not None:
-            self.shape.restitution = restitution
-    def __str__(self):
-        return "Circle(%s, colour: %s)" % (self.shape.__str__(), self.colour)
 
-class Polygon:
+
+class Polygon(Shape):
     def __init__(self, colour=(1,1,1), vertices=None,
                  density=None, friction=None, restitution=None):
-        self.shape = b2PolygonDef()
-        self.colour = colour
+        super(Polygon, self).__init__(shape=b2PolygonDef(), colour=colour,
+                                      density=density, friction=friction,
+                                      restitution=restitution)
         if vertices is not None:
             self.shape.setVertices(vertices)
-        if density is not None:
-            self.shape.density = density
-        if friction is not None:
-            self.shape.friction = friction
-        if restitution is not None:
-            self.shape.restitution = restitution
 
     def setAsBox(self, x, y):
         hx, hy = x / 2, y / 2
         self.shape.setVertices(((-hx, -hy), (hx, -hy), (hx, hy), (-hx, hy)))
-        
-    def __str__(self):
-        return "Polygon(%s, colour: %s)" % (self.shape.__str__(), self.colour)
-
